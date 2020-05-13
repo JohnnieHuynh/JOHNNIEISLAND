81872f60ac74bf1ffc2f089ec14338c2bc15c854
Johnny Huynh <44059182+JohnnieHuynh@users.noreply.github.com>
2020-05-13 15:21:50 -0700

    Initial Commit

---
+++ main.js
@@ -0,0 +1,13 @@
+
+//Smooth-Scroll Script
+
+const scroll = new SmoothScroll('.nav_links a[href*="#"]', {
+	speed: 800
+});
+
+const scroll2 = new SmoothScroll('.nav_enter a[href*="#"]', {
+	speed: 800
+});
+
+//Rain Javascript
+
